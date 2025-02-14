d568d60b1735c679773f69141bd45f58c331e95a
Oleg <evolrn89@gmail.com>
2025-02-14 21:52:42 +0600

    [hw-94] added user schema added functions checkPassword, generateToken added register user route

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,8 @@ import { Album, AlbumSchema } from './schemas/album.schema';
 import { Track, TrackSchema } from './schemas/track.schema';
 import { AlbumsController } from './albums/albums.controller';
 import { TracksController } from './tracks/tracks.controller';
+import {User, UserSchema} from "./schemas/user.schema";
+import { UsersController } from './users/users.controller';
 
 @Module({
   imports: [
@@ -16,6 +18,7 @@ import { TracksController } from './tracks/tracks.controller';
       { name: Artist.name, schema: ArtistSchema },
       { name: Album.name, schema: AlbumSchema },
       { name: Track.name, schema: TrackSchema },
+      { name: User.name, schema: UserSchema },
     ]),
   ],
   controllers: [
@@ -23,6 +26,7 @@ import { TracksController } from './tracks/tracks.controller';
     ArtistsController,
     AlbumsController,
     TracksController,
+    UsersController,
   ],
   providers: [AppService],
 })
---
+++ src/schemas/user.schema.ts
@@ -0,0 +1,57 @@
+import { Document } from 'mongoose';
+import {Prop, Schema, SchemaFactory} from "@nestjs/mongoose";
+import bcrypt from 'bcrypt';
+import {randomUUID} from "node:crypto";
+
+export interface UserDocument extends Document {
+    email: string;
+    password: string;
+    displayName: string;
+    token: string;
+    generateToken: () => void;
+    checkPassword: (password: string) => Promise<boolean>;
+}
+
+const SALT_WORK_FACTOR = 10;
+
+@Schema()
+export class User {
+    @Prop({required: true, unique: true})
+    email: string;
+
+    @Prop({required: true})
+    password: string;
+
+    @Prop({required: true})
+    token: string;
+
+    @Prop()
+    displayName: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
+
+UserSchema.methods.generateToken = function () {
+    this.token = randomUUID();
+};
+
+UserSchema.methods.checkPassword = function (password: string) {
+    return bcrypt.compare(password, this.password)
+};
+
+UserSchema.pre<UserDocument>('save', async function () {
+    if (!this.isModified('password')) return;
+
+    const salt = await bcrypt.genSalt(SALT_WORK_FACTOR);
+    this.password = await bcrypt.hash(this.password, salt);
+});
+
+UserSchema.set('toJSON', {
+    transform: (doc, ret) => {
+        delete ret.password;
+        return ret;
+    }
+});
+
+
+
